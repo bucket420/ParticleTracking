cef13dd2154a288af5b785a6748f5c86143a96cb
Chris Hoang <hoadh-23@rhodes.edu>
2023-11-13 06:02:24 -0600

    update

--- train.py
+++ train.py
@@ -2,20 +2,19 @@ import deeptrack as dt
 import numpy as np
 import os
 
-training_images = [dt.LoadImage("training_data\\" + image)()._value / 256 for image in os.listdir("training_data")]
+img = dt.LoadImage("training_data\\sample.png")()._value[:, :, :3] / 256
 model = dt.models.LodeSTAR(input_shape=(None, None, 3))
 
-for img in training_images:
-    train_set =   (
-        dt.Value(img)
-        >> dt.Add(lambda: np.random.randn() * 0.1)
-        >> dt.Gaussian(sigma=lambda:np.random.uniform(0, 0.2))  
-        >> dt.Multiply(lambda: np.random.uniform(0.6, 1.2))
-    )
-    model.fit(
-        train_set,
-        epochs=30,
-        batch_size=8,
-    )
-    
+train_set =   (
+    dt.Value(img)
+    >> dt.Add(lambda: np.random.randn() * 0.1)
+    >> dt.Gaussian(sigma=lambda:np.random.uniform(0, 0.2))  
+    >> dt.Multiply(lambda: np.random.uniform(0.6, 1.2))
+)
+model.fit(
+    train_set,
+    epochs=100,
+    batch_size=8,
+)
+
 model.save_weights("models\model\weights")
--- utils.py
+++ utils.py
@@ -3,52 +3,106 @@ import numpy as np
 import os
 import skimage
 import matplotlib.pyplot as plt
+import pandas as pd
+from laptrack import LapTrack
 
 
-def extract_frames(path, start_frame=0, stop_frame=0, invert=True, apply_func=None, step=1, *args, **kwargs):
+def extract_frames(path, invert=True, *args, **kwargs):
     video = cv.VideoCapture(path)
     num_frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
-    if stop_frame == 0:
-        stop_frame = num_frames
     frames = [None] * num_frames
-    for i in range(start_frame, stop_frame):
+    for i in range(num_frames):
         _, frame = video.read()
-        # frame = to_8bit(frame)
-        if invert: frame = skimage.util.invert(frame)
-        if i % step == 0 and callable(apply_func):
-            apply_func(path, frame, i, *args, **kwargs)
+        if invert: 
+            frame = skimage.util.invert(frame)
         frames[i] = frame / 256
     video.release()
     return frames
-
-def play(frames, stop_key=" "):
-    for frame in frames:
-        cv.imshow('Example', frame)
-        if cv.waitKey(20) & 0xFF==ord(stop_key):
-            break
-    cv.destroyAllWindows()
-    
-def save_frame(path_to_video, frame, i, target_dir=""):
-    video_name = path_to_video.split("/")[-1]
-    img_name = video_name.split(".")[0] + str(i) + ".png"
-    img_path = target_dir + "/" + img_name
-    cv.imwrite(img_path, frame)
-    
-def videos_to_images(video_folder):
-    for video in os.listdir(video_folder):
-        if video.endswith(".avi"):
-            img_folder = video_folder + video.split(".")[0]
-            if not os.path.exists(img_folder):    
-                os.mkdir(img_folder)
-            path_to_video = video_folder + video
-            print(path_to_video)
-            extract_frames(path_to_video, 0, 0, True, save_frame, 1, target_dir=img_folder)
             
 def display(img):
     fig, ax = plt.subplots()
     fig.tight_layout()
     ax.imshow(img, cmap='gray')
     
-def read(path):
-    img = cv.imread(path)
-    return img
+def detect_frame(frame, model, alpha=0.5, beta=0.5, cutoff=0.99):    
+    detections = model.predict_and_detect(frame[np.newaxis], alpha=alpha, beta=beta, cutoff=cutoff, mode="quantile")[0]        
+    return detections
+
+def detect_video(path, model, alpha=0.5, beta=0.5, cutoff=0.99):
+    frames = extract_frames(path)
+    detections = [detect_frame(frame, model, alpha=alpha, beta=beta, cutoff=cutoff) for frame in frames]
+    return detections
+
+def extract_first_frame(path, invert=True):
+    video = cv.VideoCapture(path)
+    _, frame = video.read()
+    if invert: 
+        frame = skimage.util.invert(frame)
+    frame = frame / 256
+    video.release()
+    return frame
+
+def link_particles(detected, scale, fps, min_duration=100, min_displacement=100):
+    spots = []
+    for i, frame in enumerate(detected):
+        for spot in frame:
+            spots.append([i, spot[1], spot[0]])
+    spots_df = pd.DataFrame(spots, columns=["frame", "x", "y"])
+    
+    lt = LapTrack(gap_closing_max_frame_count=5, gap_closing_cost_cutoff=400)
+    track_df, _, _ = lt.predict_dataframe(spots_df, ["x", "y"], only_coordinate_cols=False)
+    track_df = track_df.reset_index()
+    ntracks = track_df["track_id"].nunique()
+    
+    tracks = []
+    for track_id in range(ntracks):
+        track = track_df[track_df["track_id"] == track_id]
+        if len(track) < 100 or abs(track["x"].iloc[-1] - track["x"].iloc[0]) < 100:
+            continue
+        track = track[['frame', 'x', 'y']]
+        time = np.ravel(track['frame'].to_numpy()) / fps
+        x_um = np.ravel(track['x'].to_numpy()) * scale
+        y_um = np.ravel(track['y'].to_numpy()) * scale
+        
+        delta_x = [None]
+        delta_y = [None]
+        vx = [None]
+        vy = [None]
+        v = [None]
+        for i in range(len(track)):
+            if i > 0:
+                delta_x.append(x_um[i] - x_um[i-1])
+                delta_y.append(y_um[i] - y_um[i-1])
+                vx.append(delta_x[i] / time[i])
+                vy.append(delta_y[i] / time[i])
+                v.append(np.sqrt(vx[i]**2 + vy[i]**2))
+                
+        track['time'] = time
+        track['x_um'] = x_um
+        track['y_um'] = y_um
+        track['delta_x'] = delta_x
+        track['delta_y'] = delta_y
+        track['vx'] = vx
+        track['vy'] = vy
+        track['v'] = v
+        tracks.append(track)
+    
+    return tracks
+
+def track_all(path, model, scale, fps, min_duration=100, min_displacement=100, 
+              alpha=0.5, beta=0.5, cutoff=0.99):
+    raw_path = path + "\\raw"
+    os.mkdir(raw_path)
+    files = os.listdir(path)
+    files = [os.path.join(path, file) for file in files]
+    for file in files:
+        track_path = raw_path + "\\" + file.split("\\")[-1][:-4]
+        os.mkdir(track_path)
+        detected = detect_video(file, model, alpha=alpha, beta=beta, cutoff=cutoff)
+        tracks = link_particles(detected, scale=scale, fps=fps, 
+                                min_duration=min_duration, min_displacement=min_displacement)
+        for i, track in enumerate(tracks):
+            track.to_csv(track_path + "\\" + str(i) + ".csv")
+    
+            
+    
