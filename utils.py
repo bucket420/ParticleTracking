4df19c6b8c68b68612dfd8d5df0af7dc02dc4581
Chris Hoang <hoadh-23@rhodes.edu>
2024-03-14 17:55:12 -0500

    use trackpy for linking

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from laptrack import LapTrack
 import json
+import trackpy
 
 def extract_frames(path, invert=True, *args, **kwargs):
     video = cv.VideoCapture(path)
@@ -33,24 +34,18 @@ def detect_video(path, model, alpha=0.5, beta=0.5, cutoff=0.99):
     detections = [detect_frame(frame, model, alpha=alpha, beta=beta, cutoff=cutoff) for frame in frames]
     return detections
 
-def link_particles(detected, scale, fps, min_duration=100, min_displacement=100, 
-                   gap_closing_max_frame_count=5, gap_closing_cost_cutoff=225):
+def link_particles(detected, scale, fps, min_duration=100, min_displacement=100, search_range=25, memory=5):
     spots = []
     for i, frame in enumerate(detected):
         for spot in frame:
             spots.append([i, spot[1], spot[0]])
     spots_df = pd.DataFrame(spots, columns=["frame", "x", "y"])
     
-    lt = LapTrack(gap_closing_max_frame_count=gap_closing_max_frame_count, 
-                  gap_closing_cost_cutoff=gap_closing_cost_cutoff)
-    
-    track_df, _, _ = lt.predict_dataframe(spots_df, ["x", "y"], only_coordinate_cols=False)
-    track_df = track_df.reset_index()
-    ntracks = track_df["track_id"].nunique()
-    
     tracks = []
+    tracks_df = trackpy.link_df(spots_df, search_range=search_range, memory=memory)
+    ntracks = tracks_df["particle"].nunique()
     for track_id in range(ntracks):
-        track = track_df[track_df["track_id"] == track_id]
+        track = tracks_df[tracks_df["particle"] == track_id]
         if len(track) < min_duration or abs(track["x"].iloc[-1] - track["x"].iloc[0]) < min_displacement:
             continue
         track = track[['frame', 'x', 'y']]
@@ -91,21 +86,20 @@ def detect_all(path, model, alpha=0.5, beta=0.5, cutoff=0.99):
         detected = detect_video(file, model, alpha=alpha, beta=beta, cutoff=cutoff)
         np.save(detections_path + "\\" + file.split("\\")[-1][:-4] + ".npy", np.array(detected, dtype=object), allow_pickle=True)   
              
-def track_all(path, scale, fps, min_duration=100, min_displacement=100, 
-              gap_closing_max_frame_count=5, gap_closing_cost_cutoff=225):
+def track_all(path, scale, fps, min_duration=100, min_displacement=100,
+              search_range=25, memory=5):
     tracks_path = path + "\\tracks"
     detections_path = path + "\\detections"
     os.mkdir(tracks_path)
     files = os.listdir(path)
-    files = [os.path.join(path, file) for file in files]
+    files = [os.path.join(path, file) for file in files if file.endswith(".avi")]
     for file in files:
         video_tracks = tracks_path + "\\" + file.split("\\")[-1][:-4]
         os.mkdir(video_tracks)
         detected = np.load(detections_path + "\\" + file.split("\\")[-1][:-4] + ".npy", allow_pickle=True)
         tracks = link_particles(detected, scale=scale, fps=fps, 
                                 min_duration=min_duration, min_displacement=min_displacement,
-                                gap_closing_max_frame_count=gap_closing_max_frame_count,
-                                gap_closing_cost_cutoff=gap_closing_cost_cutoff)
+                                search_range=search_range, memory=memory)
         for i, track in enumerate(tracks):
             track.to_csv(video_tracks + "\\" + str(i) + ".csv")
          
